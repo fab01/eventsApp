6fd849d82eda12b99d0dc6e666e2b6cd57e98026
fab01 <info@fab01.com>
2018-05-31 23:27:55 +0200

    Update Subscription form + controller

--- app/Controllers/SubscriptionController.php
+++ app/Controllers/SubscriptionController.php
@@ -77,12 +77,68 @@ class SubscriptionController extends Controller
         //return $response->withRedirect($this->router->pathFor('event.subscription.create'));
     }
 
-    public function getEventSubscriptionUpdate($request, $response, $args) {
+    public function getEventSubscriptionUpdate($request, $response, $args)
+    {
+        $form = $this->form->getFields('EventSubscription')->updateSet($args['id']);
 
+        return $this->view->render($response, 'controller/subscription/event.html.twig',
+          [
+            'form_title'  => 'Event subscription Update',
+            'form_submit' => 'Update subscription',
+            'form_action' => 'event.subscription.update',
+            'form' => $form,
+            'id' => $args['id'],
+          ]
+        );
     }
 
-    public function postEventSubscriptionUpdate($request, $response) {
+    public function postEventSubscriptionUpdate($request, $response)
+    {
+        $uploadedFiles =  $request->getUploadedFiles();
+        $uploadedFile  =  $uploadedFiles['abstract_file'];
+        $filename      =  $uploadedFile->getClientFilename();
+        $application   =  $request->getParam('abstract_apply');
+        $abstract      =  true;
 
+        if (null != $filename || $application) {
+
+            // File extensions.
+            $file_validation = v::oneOf(
+              v::extension('pdf'),
+              v::extension('doc'),
+              v::extension('docx')
+            )->validate($filename);
+
+            // File apply for.
+            $apply_validation = v::notEmpty()->validate($application);
+
+            // Validate extensions.
+            if (!$file_validation) {
+                $this->validator->setCustomErrors(
+                  'abstract_file',
+                  'Please check the abstract file. Make sue the extension is on of the following: pdf, doc, docx'
+                );
+                $abstract = false;
+            }
+
+            // Validate application.
+            if (!$apply_validation) {
+                $this->validator->setCustomErrors(
+                  'abstract_apply',
+                  'Please select the application of the file.'
+                );
+                $abstract = false;
+            }
+        }
+
+        if (!$abstract) {
+            $this->flash->addMessage('error', "Something went wrong with the submission. Check for the errors reported in the form.");
+        }
+
+        return $response->withRedirect($this->router->pathFor('event.subscription.update', ['id' => $request->getParam('id')]));
+
+        // ELSE SUBSCRIBE USER ...
+        //return $response->withRedirect($this->router->pathFor('event.subscription.create'));
     }
 
     public function getMeetupSubscriptionCreate($request, $response, $args) {
--- app/Form/Fields/EventSubscriptionFields.php
+++ app/Form/Fields/EventSubscriptionFields.php
@@ -56,4 +56,44 @@ class EventSubscriptionFields extends Form
 
         return $fields;
     }
+
+    /**
+     * @return array
+     */
+    public function updateSet($id)
+    {
+        // CREATE - Abstract file
+        $abstract_file = F::file()->attr(
+          [
+            'name' => 'abstract_file',
+            'id' => 'my-file-selector',
+            'style' => 'display:none;',
+            'onchange' => '$(\'#upload-file-info\').html(this.files[0].name)'
+          ]
+        );
+
+        // CREATE - Abstract apply
+        $abstract_apply = F::select()
+          ->attr('name', 'abstract_apply')
+          ->options(
+            [
+              '-',
+              'abstract' => 'Apply as Abstract',
+              'talk' => 'Apply as Talk',
+            ]
+          )
+          ->addClass('form-control')
+          ->val($this->getMessage('abstract_apply'))
+          ->label('For what do you want to apply the file?');
+
+        $subscriptionId = F::Hidden()->attr(['name' => 'id'])->val($id);
+
+        $fields = [
+          'abstract_file' => $abstract_file,
+          'abstract_apply' => $abstract_apply,
+          'id' => $subscriptionId,
+        ];
+
+        return $fields;
+    }
 }
